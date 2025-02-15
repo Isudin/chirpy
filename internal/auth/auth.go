48159fb88f695f94948af4d327e3317bfea5320e
Isu <ntrojak98@gmail.com>
2025-02-15 19:00:52 +0100

    Creating JWT tokens

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -1,6 +1,12 @@
 package auth
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func HashPassword(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), 10)
@@ -13,4 +19,53 @@ func HashPassword(password string) (string, error) {
 
 func CheckPasswordHash(password, hash string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+}
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error) {
+	issuedAt := jwt.NumericDate{
+		Time: time.Now().UTC(),
+	}
+
+	expiresAt := jwt.NumericDate{
+		Time: time.Now().UTC().Add(expiresIn),
+	}
+
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		jwt.RegisteredClaims{
+			Issuer:    "chirpy",
+			IssuedAt:  &issuedAt,
+			ExpiresAt: &expiresAt,
+			Subject:   userID.String(),
+		},
+		nil,
+	)
+
+	return token.SignedString([]byte(tokenSecret))
+}
+
+func ValidateJWT(tokenString, tokenSecret string) (uuid.UUID, error) {
+	claims := jwt.MapClaims{}
+	token, err := jwt.ParseWithClaims(
+		tokenString,
+		claims,
+		func(t *jwt.Token) (interface{}, error) {
+			return []byte(tokenSecret), nil
+		},
+	)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	idStr, err := token.Claims.GetSubject()
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	id, err := uuid.Parse(idStr)
+	if err != nil {
+		return uuid.Nil, err
+	}
+
+	return id, nil
 }
--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -1,6 +1,11 @@
 package auth
 
-import "testing"
+import (
+	"testing"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 func TestHashPassword(t *testing.T) {
 	passwords := []string{"test123", "ADF123!@#fs"}
@@ -12,6 +17,44 @@ func TestHashPassword(t *testing.T) {
 
 		if CheckPasswordHash(password, hash) != nil {
 			t.Fatalf("Password '%v' doesn't match the hash '%v'", password, hash)
+		}
+	}
+}
+
+func TestJWT(t *testing.T) {
+	cases := []struct {
+		id        uuid.UUID
+		secret    string
+		expiresAt time.Duration
+	}{
+		{
+			id:        uuid.New(),
+			secret:    "Test",
+			expiresAt: time.Hour,
+		},
+		{
+			id:        uuid.New(),
+			secret:    "ttt",
+			expiresAt: time.Minute,
+		},
+	}
+
+	for i, c := range cases {
+		t.Logf("Case %v: %v, %v, %v\n", i, c.id, c.secret, c.expiresAt)
+		token, err := MakeJWT(c.id, c.secret, c.expiresAt)
+		if err != nil {
+			t.Fatalf("Error creating jwt token: %v, %v, %v - %v", c.id, c.secret, c.expiresAt, err)
+		}
+
+		t.Logf("Token: %v\n", token)
+		id, err := ValidateJWT(token, c.secret)
+		if err != nil {
+			t.Fatalf("Error validating token: %v, %v - %v", token, c.secret, err)
+		}
+
+		t.Logf("ID: %v\n", id)
+		if c.id != id {
+			t.Fatalf("Id's doesn't match: %v != %v", c.id, id)
 		}
 	}
 }
